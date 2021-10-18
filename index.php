a6e726733193b66782fcae9a48f15044202630e5
AMYSoft <info@amy98.ir>
2021-10-18 14:07:01 +0330

    Update code.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ ROUTE::get("/home/" , function () {
     include(__DIR__ . "/assets/pages/home.php");
 });
 ?>
-<div class="four-sado-four-container container">
-<h1 style="color: crimson;">404</h1>
+<div style="text-align: center;" class="four-sado-four-container container">
+  <h1 style="color: crimson;font-size: 500;margin: 0;">404</h1>
+  <h3 style="color: crimson;font-size: 50;">You can't get this folder from this server!</h3>
 </div>
