dffa1041232a97e2fabfdcfa8def4e66f33dad48
AMYSoft <info@amy98.ir>
2021-10-26 13:23:25 +0330

    Update code.

--- assets/pages/index.php
+++ assets/pages/index.php
@@ -8,8 +8,8 @@ button{
 </style>
 <body class="bg-light text-center">
   <?php include(__DIR__ . "/menu.php"); ?>
-  <div class="main" style="display: inline;padding-top: 60px;position: absolute;left: 50%;transform: translateX(-50%);bottom: 0;">
-    <iframe class="rsampad-iframe" style="width: 100vw;height: calc(100vh - 55px);" class="rsampad-iframe" src="https://rsampad.ir" frameborder="0"></iframe>
+  <div class="main" style="">
+    <iframe class="rsampad-iframe" style="width: calc(100vw);height: calc(100vh - 60px);" class="rsampad-iframe" src="https://rsampad.ir" frameborder="0"></iframe>
   </div>
 </body>
 </html>
--- assets/pages/send.php
+++ assets/pages/send.php
@@ -1,8 +1,11 @@
 <?php
 include(__DIR__ . "/../php/config.php");
+?>
+<body class="bg-light text-center">
+<?php
 include(__DIR__ . "/menu.php");
 ?>
-<div class="container form-signin main text-center p-1">
+<div class="form-signin main p-1">
   <img class="mb-4" src="/favicon.ico" alt="" width="150px">
   <h1 class="h4 mb-3 fw-normal">برای ارسال درخواست امتیاز فرم زیر را پر کنید</h1>
   <div class="form-floating">
@@ -15,51 +18,3 @@ include(__DIR__ . "/menu.php");
   </div>
   <button onclick="sendScoreRequest();" id="scoreSub" class="w-100 btn btn-lg btn-primary">ارسال</button>
   <p class="mt-5 mb-3 text-muted" dir="ltr">©1400</p>
-</div>
-
-<style>
-html,
-body {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  padding-top: 60px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
-}
-
-.form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
-
-.form-signin .checkbox {
-  font-weight: 400;
-}
-
-.form-signin .form-floating:focus-within {
-  z-index: 2;
-}
-
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-
-#floatingInput{
-  border-bottom-left-radius: unset!important;
-  border-bottom-right-radius: unset!important;
-}
-</style>
